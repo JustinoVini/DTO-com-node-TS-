bee904db2452858ac35869d8c84ada77b148d62b
Vinicius Justino <visilva5690@hotmail.com>
2023-04-04 22:43:21 -0300

    configuração do sistema

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// importação do express no sistema.
+import express, { Application, Request, Response } from 'express';
+
+// inicialização da variavel que vai receber os valores do express no sistema 
+const app: Application = express();
+
